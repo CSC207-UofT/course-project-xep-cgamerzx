858ce8b77737ab8b219020f8b0fcd09a2e7541e5
hk21702 <hk21702@users.noreply.github.com>
2021-10-11 01:36:48 -0400

    package refactoring

--- app/src/main/java/com/xepicgamerzx/hotelier/FirstFragment.java
+++ app/src/main/java/com/xepicgamerzx/hotelier/FirstFragment.java
@@ -21,6 +21,7 @@ public class FirstFragment extends Fragment {
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
+
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
