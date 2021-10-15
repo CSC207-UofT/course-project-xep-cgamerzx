fcc0edceb2b0221bbf4415e45cd7b4b03e21dc44
hk21702 <hk21702@users.noreply.github.com>
2021-10-15 10:26:00 -0400

    updated basic customer forms, structure for management forms

---
+++ app/src/main/java/com/xepicgamerzx/hotelier/customer/RoomsReturnFragment.java
@@ -0,0 +1,23 @@
+package com.xepicgamerzx.hotelier.customer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.xepicgamerzx.hotelier.R;
+
+class RoomsReturnFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_rooms_return, container, false);
+    }
+}
