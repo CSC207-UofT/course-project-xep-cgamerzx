9283e1a500dd3d66b553c834940abd2e29ffb67d
hk21702 <hk21702@users.noreply.github.com>
2021-10-15 04:48:34 -0400

    made customer and management skeleton pages

---
+++ app/src/main/java/com/xepicgamerzx/hotelier/customer/CustomerActivity.java
@@ -0,0 +1,16 @@
+package com.xepicgamerzx.hotelier.customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.xepicgamerzx.hotelier.R;
+
+public class CustomerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer);
+    }
+}
