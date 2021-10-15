901e4fe5a97e682f913972d4987f18f7bfee78f1
hk21702 <hk21702@users.noreply.github.com>
2021-10-15 04:48:34 -0400

    made customer and management skeleton pages

---
+++ app/src/main/java/com/xepicgamerzx/hotelier/management/ManagementActivity.java
@@ -0,0 +1,17 @@
+package com.xepicgamerzx.hotelier.management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.xepicgamerzx.hotelier.R;
+
+
+public class ManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_management);
+    }
+}
