bdb3a4db362626d63be0725200d22aa3748ff16e
hk21702 <hk21702@users.noreply.github.com>
2021-10-15 10:26:00 -0400

    updated basic customer forms, structure for management forms

--- app/src/main/java/com/xepicgamerzx/hotelier/customer/CustomerActivity.java
+++ app/src/main/java/com/xepicgamerzx/hotelier/customer/CustomerActivity.java
@@ -1,16 +1,47 @@
 package com.xepicgamerzx.hotelier.customer;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.Pair;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.xepicgamerzx.hotelier.R;
 
 public class CustomerActivity extends AppCompatActivity {
+
+    private Button datePickerBtn;
+
+    private TextView selectedDateRangeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
+
+        datePickerBtn = findViewById(R.id.select_dates_btn);
+        selectedDateRangeText = findViewById(R.id.selected_date_range_txt);
+
+        MaterialDatePicker.Builder<Pair<Long, Long>> builder = MaterialDatePicker.Builder.dateRangePicker();
+        builder.setTitleText("SELECT A CHECK IN AND CHECKOUT DATE");
+        final MaterialDatePicker materialDatePicker = builder.build();
+
+        datePickerBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                materialDatePicker.show(getSupportFragmentManager(),  "DATE_RANGE_PICKER");
+            }
+        });
+
+        materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+        selectedDateRangeText.setText(materialDatePicker.getHeaderText());
+            }
+        });
     }
 }
