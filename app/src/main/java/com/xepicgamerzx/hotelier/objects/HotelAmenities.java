cf07063011ffe82bc6b60483c1dfd4f6b7669729
hk21702 <hk21702@users.noreply.github.com>
2021-10-16 18:01:01 -0400

    Populate enums

--- app/src/main/java/com/xepicgamerzx/hotelier/objects/HotelAmenities.java
+++ app/src/main/java/com/xepicgamerzx/hotelier/objects/HotelAmenities.java
@@ -1,4 +1,28 @@
 package com.xepicgamerzx.hotelier.objects;
 
 public enum HotelAmenities {
+    INDOOR_POOL("Indoor Pool"),
+    OUTDOOR_POOL("Outdoor Pool"),
+    GYM("Gym"),
+    LAUNDRY("Laundry"),
+    BUSINESS_SERVICES("Business Services"),
+    WEDDING_SERVICES("Wedding Services"),
+    CONFERENCE_SPACE("Conference Space"),
+    SMOKE_FREE("Smoke Free Property"),
+    BAR("Bar"),
+    COMPLIMENTARY_BREAKFAST("Complimentary Breakfast"),
+    TFS_FRONT_DESK("24/7 Front Desk"),
+    PARKING_INCLUDED("Parking Included"),
+    RESTAURANT("Restaurant"),
+    SPA("Spa"),
+    ELEVATOR("Elevator"),
+    ATM_BANK("ATM/Banking Services"),
+    FRONT_DESK_SAFE("Front Desk Safe");
+
+
+    public final String label;
+
+    HotelAmenities(String label){
+        this.label = label;
+    }
 }
--- app/src/main/java/com/xepicgamerzx/hotelier/objects/RoomAmenities.java
+++ app/src/main/java/com/xepicgamerzx/hotelier/objects/RoomAmenities.java
@@ -1,4 +1,26 @@
 package com.xepicgamerzx.hotelier.objects;
 
 public enum RoomAmenities {
+    WIFI("Wi-Fi"),
+    ROOM_SERVICE("Room Service"),
+    SMOKING("Smoking Room"),
+    NONSMOKING("Non-smoking Room"),
+    AIR_CONDITIONING("Air conditioning"),
+    HOUSEKEEPING("House Keeping"),
+    MICROWAVE("Microwave"),
+    MINI_FRIDGE("Mini-fridge"),
+    COFFEEMAKER("Coffee Maker"),
+    UPPER_FLOOR("Upper Floor"),
+    GROUND_FLOOR("Ground Floor"),
+    WHEELCHAIR_ACCESSIBLE("Wheelchair Accessible"),
+    TV("Television"),
+    PATIO("Patio"),
+    WHIRLPOOL("Whirlpool"),
+    SAFE("Safe");
+
+    public final String label;
+
+    RoomAmenities(String label){
+        this.label = label;
+    }
 }
