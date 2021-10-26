1bb4773b3e1fe6ecfe97260f3eef957cd2cb9088
Rafee Rahman <59676643+rafeerahman@users.noreply.github.com>
2021-10-26 03:45:58 -0400

    New home page

---
+++ app/src/main/java/com/xepicgamerzx/hotelier/customer/new_home_page.java
@@ -0,0 +1,14 @@
+package com.xepicgamerzx.hotelier.customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class new_home_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_home_page);
+    }
+}
