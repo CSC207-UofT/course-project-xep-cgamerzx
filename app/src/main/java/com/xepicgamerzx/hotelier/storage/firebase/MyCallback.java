e1538ef3e2292c20a8b87976911251a3e5aa5cd1
hk21702 <hk21702@users.noreply.github.com>
2021-11-22 19:08:42 -0500

    remove firebase stuff

--- app/src/main/java/com/xepicgamerzx/hotelier/storage/firebase/MyCallback.java
+++
@@ -1,7 +0,0 @@
-package com.xepicgamerzx.hotelier.storage.firebase;
-
-import com.xepicgamerzx.hotelier.objects.OldObjects.HotelOld;
-
-public interface MyCallback {
-    void onCallback(HotelOld value);
-}
