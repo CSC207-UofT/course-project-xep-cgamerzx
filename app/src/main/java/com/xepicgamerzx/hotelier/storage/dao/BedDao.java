d67ff8e2db3d6e0319a1f11fe90296d8a5800773
hk21702 <hk21702@users.noreply.github.com>
2021-11-22 19:00:31 -0500

    documentation for daos

--- app/src/main/java/com/xepicgamerzx/hotelier/storage/dao/BedDao.java
+++ app/src/main/java/com/xepicgamerzx/hotelier/storage/dao/BedDao.java
@@ -9,14 +9,33 @@ import com.xepicgamerzx.hotelier.objects.relations.BedWithRooms;
 
 import java.util.List;
 
+/**
+ * Data access object for Beds
+ */
 @Dao
 public interface BedDao extends BaseDao<Void, Bed>{
+    /**
+     * Get all beds in Bed table.
+     *
+     * @return List<Bed> list of all beds in bed table.
+     */
     @Query("SELECT * FROM Bed")
     List<Bed> getAll();
 
+    /**
+     * Get beds with matching unique IDs in bed table.
+     *
+     * @param bedID String unique ID of bed.
+     * @return List<Bed> list of all beds with unique IDs that match bedID.
+     */
     @Query("SELECT * FROM Bed WHERE uniqueId IN (:bedID)")
     List<Bed> getBeds(String... bedID);
 
+    /**
+     * Get all beds associated with a room.
+     *
+     * @return List<BedWithRooms> list of BedWithRooms objects available.
+     */
     @Transaction
     @Query("SELECT * FROM Bed")
     List<BedWithRooms> getBedsWithRoom();
--- app/src/main/java/com/xepicgamerzx/hotelier/storage/dao/BedRoomCrossDao.java
+++ app/src/main/java/com/xepicgamerzx/hotelier/storage/dao/BedRoomCrossDao.java
@@ -7,23 +7,62 @@ import com.xepicgamerzx.hotelier.objects.cross_reference_objects.RoomBedsCrossRe
 
 import java.util.List;
 
+/**
+ * Data access object for BedRoomCrossRefs
+ */
 @Dao
 public interface BedRoomCrossDao extends BaseDao<Void, RoomBedsCrossRef>{
+    /**
+     * Get all RoomBedsCrossRef in RoomBedsCrossRef table.
+     *
+     * @return List<RoomBedsCrossRef> list of all RoomBedsCrossRef in RoomBedsCrossRef table.
+     */
     @Query("SELECT * FROM RoomBedsCrossRef")
     List<RoomBedsCrossRef> getAll();
 
+    /**
+     * Get all room IDs in RoomBedsCrossRef table with matching bed ID.
+     *
+     * @param bedID String bedID referring to unique ID of bed.
+     * @return List<Long> room IDs related to bedID.
+     */
     @Query("SELECT roomID FROM RoomBedsCrossRef WHERE uniqueId =:bedID")
     List<Long> getWith(String bedID);
 
+    /**
+     * Get all room IDs in RoomBedsCrossRef table with matching bed ID with a min count.
+     *
+     * @param bedID String bedID referring to unique ID of bed.
+     * @param count int minimum number of beds in cross ref.
+     * @return List<Long> room IDs related to count number of bedID.
+     */
     @Query("SELECT roomID FROM RoomBedsCrossRef WHERE uniqueId =:bedID AND bedCount >= :count" )
     List<Long> getWith(String bedID, int count);
 
+    /**
+     * Get all bedIDs in RoomBedsCrossRef table with matching room IDs
+     *
+     * @param roomID long room ID.
+     * @return List<String> bed IDs related to roomID
+     */
     @Query("SELECT uniqueId FROM RoomBedsCrossRef WHERE roomID =:roomID")
     List<String> getWith(long roomID);
 
+    /**
+     * Get all RoomBedsCrossRefs in RoomBedsCrossRef table with matching room IDs
+     *
+     * @param roomID long room ID.
+     * @return List<RoomBedsCrossRefs> room bed cross refs related to roomID.
+     */
     @Query("SELECT * FROM RoomBedsCrossRef WHERE roomID =:roomID")
     List<RoomBedsCrossRef> getCrossWith(long roomID);
 
+    /**
+     * Get all RoomBedsCrossRefs in RoomBedsCrossRef table with matching bed IDs
+     *
+     * @param bedID String bedID referring to unique ID of bed.
+     * @return List<RoomBedsCrossRefs> room bed cross refs related to bedID.
+     */
     @Query("SELECT * FROM RoomBedsCrossRef WHERE uniqueId =:bedID")
     List<RoomBedsCrossRef> getCrossWith (String bedID);
 }
