f0d9f1656997005932287b44112c9ba343790744
hk21702 <hk21702@users.noreply.github.com>
2021-11-22 19:11:23 -0500

    annotate clear tables dao as depreciated

--- app/src/main/java/com/xepicgamerzx/hotelier/storage/dao/ClearTablesDao.java
+++ app/src/main/java/com/xepicgamerzx/hotelier/storage/dao/ClearTablesDao.java
@@ -3,6 +3,7 @@ package com.xepicgamerzx.hotelier.storage.dao;
 import androidx.room.Dao;
 import androidx.room.Query;
 
+@Deprecated // I don't think this is being used?
 @Dao
 public interface ClearTablesDao {
 
