163c792b94a89e1fcda245c068d85993d063d590
hk21702 <hk21702@users.noreply.github.com>
2021-11-24 14:38:47 -0500

    add cosine and sine representations of long lat to Address

--- app/src/main/java/com/xepicgamerzx/hotelier/objects/hotel_objects/Address.java
+++ app/src/main/java/com/xepicgamerzx/hotelier/objects/hotel_objects/Address.java
@@ -1,35 +1,72 @@
 package com.xepicgamerzx.hotelier.objects.hotel_objects;
 
 import androidx.annotation.NonNull;
+import androidx.room.Ignore;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 public class Address implements Serializable {
 
-    public String postalCode;
+    private final String postalCode;
     private final String streetName;
     private final String streetNumber;
     private final String city;
     private final String province;
     private final double longitude;
     private final double latitude;
+    private final double lonCos;
+    private final double lonSin;
+    private final double latCos;
+    private final double latSin;
 
     /**
-     * Creates a new Address
+     * Create a new address
      *
-     * @param streetName The name of the street
-     * @param postalCode The postal code
+     * @param streetName String name of street
+     * @param postalCode String postal code
+     * @param streetNumber String street number
+     * @param city String city name
+     * @param province String province/state name
+     * @param longitude double longitude
+     * @param latitude double latitude
      */
+    @Ignore
     public Address(String streetName, String postalCode, String streetNumber, String city,
                    String province, double longitude, double latitude) {
-        this.streetName = streetName;
+        this(postalCode, streetName, streetNumber, city, province, longitude, latitude,
+                Math.cos(latitude * Math.PI / 180),
+                Math.sin(latitude * Math.PI / 180),
+                Math.cos(latitude * Math.PI / 180),
+                Math.sin(latitude * Math.PI / 180));
+    }
+
+    /**
+     * Create a new address
+     *
+     * @param streetName String name of street
+     * @param postalCode String postal code
+     * @param streetNumber String street number
+     * @param city String city name
+     * @param province String province/state name
+     * @param longitude double longitude
+     * @param latitude double latitude
+     * @param lonCos double cosine representation of longitude
+     * @param lonSin double sine representation of longitude
+     * @param latCos double cosine representation of latitude
+     * @param latSin double sine representation of latitude
+     */
+    public Address(String postalCode, String streetName, String streetNumber, String city, String province, double longitude, double latitude, double lonCos, double lonSin, double latCos, double latSin) {
         this.postalCode = postalCode;
+        this.streetName = streetName;
         this.streetNumber = streetNumber;
         this.city = city;
         this.province = province;
         this.longitude = longitude;
         this.latitude = latitude;
+        this.lonCos = lonCos;
+        this.lonSin = lonSin;
+        this.latCos = latCos;
+        this.latSin = latSin;
     }
 
     public String getStreetNumber() {
@@ -64,6 +101,22 @@ public class Address implements Serializable {
         return String.format("%s %s", this.streetNumber, this.streetName);
     }
 
+    public double getLonCos() {
+        return lonCos;
+    }
+
+    public double getLonSin() {
+        return lonSin;
+    }
+
+    public double getLatCos() {
+        return latCos;
+    }
+
+    public double getLatSin() {
+        return latSin;
+    }
+
     @NonNull
     @Override
     public String toString() {
@@ -79,20 +132,49 @@ public class Address implements Serializable {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Address)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
+
         Address address = (Address) o;
-        return Double.compare(address.getLongitude(), getLongitude()) == 0 &&
-                Double.compare(address.getLatitude(), getLatitude()) == 0 &&
-                getStreetName().equals(address.getStreetName()) &&
-                getPostalCode().equals(address.getPostalCode()) &&
-                getStreetNumber().equals(address.getStreetNumber()) &&
-                getCity().equals(address.getCity()) &&
-                getProvince().equals(address.getProvince());
+
+        if (Double.compare(address.getLongitude(), getLongitude()) != 0) return false;
+        if (Double.compare(address.getLatitude(), getLatitude()) != 0) return false;
+        if (Double.compare(address.getLonCos(), getLonCos()) != 0) return false;
+        if (Double.compare(address.getLonSin(), getLonSin()) != 0) return false;
+        if (Double.compare(address.getLatCos(), getLatCos()) != 0) return false;
+        if (Double.compare(address.getLatSin(), getLatSin()) != 0) return false;
+        if (getPostalCode() != null ? !getPostalCode().equals(address.getPostalCode()) : address.getPostalCode() != null)
+            return false;
+        if (getStreetName() != null ? !getStreetName().equals(address.getStreetName()) : address.getStreetName() != null)
+            return false;
+        if (getStreetNumber() != null ? !getStreetNumber().equals(address.getStreetNumber()) : address.getStreetNumber() != null)
+            return false;
+        if (getCity() != null ? !getCity().equals(address.getCity()) : address.getCity() != null)
+            return false;
+        return getProvince() != null ? getProvince().equals(address.getProvince()) : address.getProvince() == null;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getStreetName(), getPostalCode(), getStreetNumber(), getCity(), getProvince(), getLongitude(), getLatitude());
+        int result;
+        long temp;
+        result = getPostalCode() != null ? getPostalCode().hashCode() : 0;
+        result = 31 * result + (getStreetName() != null ? getStreetName().hashCode() : 0);
+        result = 31 * result + (getStreetNumber() != null ? getStreetNumber().hashCode() : 0);
+        result = 31 * result + (getCity() != null ? getCity().hashCode() : 0);
+        result = 31 * result + (getProvince() != null ? getProvince().hashCode() : 0);
+        temp = Double.doubleToLongBits(getLongitude());
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(getLatitude());
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(getLonCos());
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(getLonSin());
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(getLatCos());
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(getLatSin());
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
     }
 }
 
