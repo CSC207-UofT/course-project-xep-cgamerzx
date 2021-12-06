9c07c8f01074c2bb40c2fcddf9995148ac22922f
Thomas <tomtom.liu@mail.utoronto.ca>
2021-12-06 17:10:41 -0500

    Updated branch

--- app/src/main/java/com/xepicgamerzx/hotelier/management_hotel_listing_activity/HotelCreatorActivity.java
+++ app/src/main/java/com/xepicgamerzx/hotelier/management_hotel_listing_activity/HotelCreatorActivity.java
@@ -11,27 +11,28 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
-import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.textfield.TextInputEditText;
 import com.xepicgamerzx.hotelier.R;
 import com.xepicgamerzx.hotelier.objects.hotel_objects.Address;
-import com.xepicgamerzx.hotelier.objects.hotel_objects.Amenity;
 import com.xepicgamerzx.hotelier.objects.hotel_objects.HotelAmenity;
 import com.xepicgamerzx.hotelier.objects.hotel_objects.HotelRoom;
 import com.xepicgamerzx.hotelier.storage.Manage;
 
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class HotelCreatorActivity extends AppCompatActivity {
+    String text = "Hotel Details:";
     Manage manage;
-
     Address address;
     List<HotelRoom> hotelRooms = new ArrayList<>();
     List<HotelAmenity> hotelAmenities = new ArrayList<>();
+    TextInputEditText hotelName;
+    MaterialButton addAddressBtn, addRoomsBtn, addAmenitiesBtn, submitBtn, hotelDetails;
+    ImageButton backBtn;
+    boolean isRoomsMade = false, isAddressMade = false, isHotelNameMade = false;
 
     boolean[] selectedAmenity;
     ArrayList<Integer> amenitiesList = new ArrayList<>();
@@ -39,18 +40,6 @@ public class HotelCreatorActivity extends AppCompatActivity {
             "Business Services", "Wedding Services", "Coneference Space", "Smoke Free Property",
             "Bar", "Complementary Breakfast", "24/7 Front Desk", "Parking Included", "Restaurant",
             "Spa", "Elevator", "ATM/Banking Services", "Front Desk Safe"};
-
-    TextInputEditText hotelName;
-    MaterialButton addAddressBtn;
-    MaterialButton addRoomsBtn;
-    MaterialButton amenitiesMenu;
-    MaterialButton submitBtn;
-    MaterialButton hotelDetails;
-    ImageButton backBtn;
-    boolean isRoomsMade = false;
-    boolean isAddressMade = false;
-    boolean isHotelNameMade = false;
-    String text = "Hotel Details:";
     private AlertDialog.Builder dialogBuilder;
     private AlertDialog dialog;
 
@@ -62,15 +51,20 @@ public class HotelCreatorActivity extends AppCompatActivity {
 
         // Initializing db
         initializeDb();
+        setCreationFields();
+        callAllListeners();
+    }
 
-        hotelName = findViewById(R.id.hotelNameInput);
-        addAddressBtn = findViewById(R.id.addAddressBtn);
-        addRoomsBtn = findViewById(R.id.addRoomsBtn);
-        amenitiesMenu = findViewById(R.id.hotelAmenities);
-        submitBtn = findViewById(R.id.saveHotelBtn);
-        backBtn = findViewById(R.id.backBtn);
-        hotelDetails = findViewById(R.id.hotelDetails);
+    public void callAllListeners() {
+        hotelDetails.setOnClickListener(v -> createHotelInfoDialog());
+        backBtn.setOnClickListener(v -> HotelCreatorActivity.super.onBackPressed());
+        submitListener();
+        addressClickListener();
+        roomsClickListener();
+        amenitiesClickListener();
+    }
 
+    public void submitListener() {
         hotelDetails.setOnClickListener(v -> createHotelInfoDialog());
 
         submitBtn.setOnClickListener(new View.OnClickListener() {
@@ -89,39 +83,14 @@ public class HotelCreatorActivity extends AppCompatActivity {
 
             }
         });
+    }
 
-        addAddressBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getSupportFragmentManager().beginTransaction()
-                        .add(R.id.hotelCreator, HotelCreateAddressFragment.class, null)
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });
-
-        addRoomsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getSupportFragmentManager().beginTransaction()
-                        .add(R.id.hotelCreator, HotelCreateRoomsFragment.class, null)
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });
-
-        backBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                HotelCreatorActivity.super.onBackPressed();
-            }
-        });
-
+    public void amenitiesClickListener() {
         //Create a dropdown menu to select amenities
         //Initialize selected amenity array
         selectedAmenity = new boolean[amenitiesArray.length];
 
-        amenitiesMenu.setOnClickListener(new View.OnClickListener() {
+        addAmenitiesBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Initialize alert dialog
@@ -165,7 +134,7 @@ public class HotelCreatorActivity extends AppCompatActivity {
                             }
                         }
                         //Set text on text view
-                        amenitiesMenu.setText(stringBuilder.toString());
+                        addAmenitiesBtn.setText(stringBuilder.toString());
                     }
                 });
 
@@ -184,7 +153,7 @@ public class HotelCreatorActivity extends AppCompatActivity {
                             //Remove all selection
                             selectedAmenity[i] = false;
                             amenitiesList.clear();
-                            amenitiesMenu.setText("Add amenities");
+                            addAmenitiesBtn.setText("Add amenities");
                         }
                     }
                 });
@@ -192,6 +161,40 @@ public class HotelCreatorActivity extends AppCompatActivity {
                 builder.show();
             }
         });
+    }
+
+    public void addressClickListener() {
+        addAddressBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.hotelCreator, HotelCreateAddressFragment.class, null)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+
+    public void roomsClickListener() {
+        addRoomsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.hotelCreator, HotelCreateRoomsFragment.class, null)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+
+    public void setCreationFields() {
+        hotelName = findViewById(R.id.hotelNameInput);
+        addAddressBtn = findViewById(R.id.addAddressBtn);
+        addRoomsBtn = findViewById(R.id.addRoomsBtn);
+        addAmenitiesBtn = findViewById(R.id.hotelAmenitiesBtn);
+        submitBtn = findViewById(R.id.saveHotelBtn);
+        backBtn = findViewById(R.id.backBtn);
+        hotelDetails = findViewById(R.id.hotelDetails);
     }
 
     public void initializeDb() {
